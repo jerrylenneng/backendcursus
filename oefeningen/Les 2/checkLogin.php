390fc9459e7212ce3a1eb8161f1f2433716076e5
jerrylenneng <jtandirau@gmail.com>
2021-04-16 00:25:48 +0200

    Create checkLogin.php

---
+++ oefeningen/Les 2/checkLogin.php
@@ -0,0 +1,63 @@
+<html>
+<body>
+
+<?php
+
+define('DB_HOST',"db");
+define('DB_PORT', "3306"); // 8889 voor mamp mysql, 3306 voor docker mysql
+define('DB_USERNAME',"avans"); 
+define('DB_PASSWORD',"plus");
+define('DB_DATABASE',"phpcursus"); 
+
+$connection = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT);
+
+if (!$connection) {
+	die('Verbinding met de database is mislukt.');
+}
+else {
+	echo 'Verbinding met de database is gelukt';
+}
+
+
+
+echo "<br><br>";
+
+
+
+
+
+
+if(isset($_POST["username"], $_POST["password"])) 
+    {     
+
+        $username = $_POST["username"]; 
+        $password = $_POST["password"]; 
+
+		$result1 = $connection->query("SELECT username, password FROM users WHERE username = '$username' AND password = '$password'");
+
+		if ($result1->num_rows > 0)
+     
+        { 
+			echo 'Hallo admin!';
+			
+			
+        }
+        else
+        {
+            echo 'Wachtwoord en/of username is fout!!';
+        }
+}
+
+
+
+
+
+?>
+
+
+
+
+
+
+</body>
+</html>
