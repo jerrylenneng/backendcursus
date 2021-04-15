1915f1251c6395156467d0e24402bb294fdcec8d
jerrylenneng <jtandirau@gmail.com>
2021-04-16 00:25:57 +0200

    Create login.php

---
+++ oefeningen/Les 2/login.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+<form action="checkLogin.php" method="post">
+Name: <input type="text" name="username">
+<br>
+Password: <input type="password" name="password">
+<br>
+<input type="submit" value="Login" >
+</form>
+
+</body>
+</html> 
