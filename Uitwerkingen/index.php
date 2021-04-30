1195d3cb40e57814c8f174733c347bb3b98ef67e
jerrylenneng <jtandirau@gmail.com>
2021-04-30 09:02:47 +0200

    Create index.php

---
+++ Uitwerkingen/index.php
@@ -0,0 +1,20 @@
+
+
+<h1> Oefeningen en eindopdrachten </h1>
+
+<h4> Klik hier voor les 1: </h4><a href="Les 1/oefeningenLes1.php"> Les 1 </a>
+<h4> Klik hier voor les 2: </h4><a href="Les 2/oefeningenLes2.php"> Les 2 </a>
+<h4> Klik hier voor les 3: </h4><a href="Les 3/oefeningenLes3.php"> Les 3 </a>
+<h4> Klik hier voor de eindopdracht </h4><a href="Eindopdracht/eindopdracht.php"> Eindopdracht </a>
+
+
+
+
+
+
+
+
+
+	
+
+
