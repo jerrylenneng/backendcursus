0c637cb184c7bb06f082912b2753e3d8f89f075f
jerrylenneng <jtandirau@gmail.com>
2021-04-30 09:03:04 +0200

    Create config.php

---
+++ Uitwerkingen/Les 2/config.php
@@ -0,0 +1,16 @@
+<?php
+
+define('DB_HOST',"db");
+define('DB_PORT', "3306"); // 8889 voor mamp mysql, 3306 voor docker mysql
+define('DB_USERNAME',"avans"); 
+define('DB_PASSWORD',"plus");
+define('DB_DATABASE',"phpcursus"); 
+ 
+$connection = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT);
+
+if (!$connection) {
+	die('Verbinding met de database is mislukt.');
+}
+else {
+	echo 'Verbinding gelukt';
+}
