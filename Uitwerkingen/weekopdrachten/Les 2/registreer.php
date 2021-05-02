8fd05eba4118349ad0937870786464aab3418928
jerrylenneng <jtandirau@gmail.com>
2021-05-02 20:14:55 +0200

    Create registreer.php

---
+++ Uitwerkingen/weekopdrachten/Les 2/registreer.php
@@ -0,0 +1,17 @@
+<body>
+
+<form id="form" action="registreerUser.php" method="post">
+    <label for="name">Gebruikersnaam</label>
+    <input type="text" id="name" name="name" placeholder="Naam...">
+	<br>
+    <label for="email">Wachtwoord</label>
+    <input type="text" id="email" name="email" placeholder="Wachtwoord...">
+	<br>
+	<label for="is_admin">Admin? </label>
+	<input type="checkbox" id="is_admin" >
+    <input type="submit" value="Submit">
+</form>
+
+
+
+</body>
