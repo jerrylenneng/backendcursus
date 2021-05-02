277a0ce78d74f9e2e824e6cc70d7617cbe817352
jerrylenneng <jtandirau@gmail.com>
2021-05-02 20:13:08 +0200

    Update index.php

--- Uitwerkingen/index.php
+++ Uitwerkingen/index.php
@@ -2,10 +2,23 @@
 
 <h1> Oefeningen en eindopdrachten </h1>
 
-<h4> Klik hier voor les 1: </h4><a href="Les 1/oefeningenLes1.php"> Les 1 </a>
-<h4> Klik hier voor les 2: </h4><a href="Les 2/oefeningenLes2.php"> Les 2 </a>
-<h4> Klik hier voor les 3: </h4><a href="Les 3/oefeningenLes3.php"> Les 3 </a>
-<h4> Klik hier voor de eindopdracht </h4><a href="Eindopdracht/eindopdracht.php"> Eindopdracht </a>
+<h4> Klik hier voor les 1: </h4>
+<a href="weekopdrachten/Les 1/oefeningenLes1.php"> Les 1 Oefeningen</a> <br>
+<a href="weekopdrachten/Les 1/weekopdrachtLes1.php"> Weekopdracht</a>
+
+<h4> Klik hier voor les 2: </h4>
+<a href="weekopdrachten/Les 2/oefeningenLes2.php"> Les 2 Oefeningen</a> <br>
+<a href="weekopdrachten/Les 2/weekopdrachtLes2.php"> Weekopdracht</a>
+
+<h4> Klik hier voor les 3: </h4>
+<a href="weekopdrachten/Les 3/oefeningenLes3.php"> Les 3 Oefeningen</a> <br>
+<a href="weekopdrachten/Les 3/weekopdrachtLes3.php"> Weekopdracht</a>
+
+<h4> Klik hier voor les 4: </h4>
+<a href="weekopdrachten/Les 4/oefeningenLes4.php"> Les 4 Oefeningen</a> <br>
+<a href="weekopdrachten/Les 4/weekopdrachtLes4.php"> Weekopdracht</a>
+
+<h4> Klik hier voor de eindopdracht </h4><a href="eindopdracht/eindopdracht.php"> Eindopdracht </a>
 
 
 
