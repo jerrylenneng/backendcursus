c677931b3cac318fd29c7ddca53dcd7ef1f23bf5
jerrylenneng <jtandirau@gmail.com>
2021-05-02 20:14:50 +0200

    Create menu.php

---
+++ Uitwerkingen/weekopdrachten/Les 2/menu.php
@@ -0,0 +1,25 @@
+<body>
+
+<ul id="menu" class="menu">
+	<li><a href="index.php"><img src="/img/logo.jpg" alt="logo avans+" width="50" /></a></li>
+	<li><a href="index.php">Naar theorie</a></li>
+</ul><form id="form" action="comment.php" method="post">
+    <label for="name">Name</label>
+    <input type="text" id="name" name="name" placeholder="Your name..">
+
+    <label for="email">Email</label>
+    <input type="text" id="email" name="email" placeholder="Your email">
+
+  	 <label for="website">Website</label>
+    <input type="text" id="website" name="website" placeholder="Your website url">
+
+    <label for="message">Message</label>
+
+    <textarea id="message" name="message" placeholder="Write something.." style="height:200px"></textarea>
+
+    <input type="submit" value="Submit">
+</form>
+
+
+
+</body>
