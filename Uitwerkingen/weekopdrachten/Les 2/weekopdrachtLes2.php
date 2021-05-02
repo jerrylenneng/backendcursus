2a4b32610534efae89564c908425c8970b18d8d6
jerrylenneng <jtandirau@gmail.com>
2021-05-02 20:15:02 +0200

    Create weekopdrachtLes2.php

---
+++ Uitwerkingen/weekopdrachten/Les 2/weekopdrachtLes2.php
@@ -0,0 +1,24 @@
+<html>
+<body>
+
+
+<h1> Weekopdracht </h1>
+
+<h3> Login </h3>
+
+<form action="checkLogin.php" method="post">
+Name: <input type="text" name="username">
+<br>
+Password: <input type="password" name="password">
+<br>
+<input type="submit" value="Login" >
+</form>
+	
+
+
+
+
+
+
+</body>
+</html>
